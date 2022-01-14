213504ba3637f39d959639253cffbbafd707ded2
Charles Aggasid <charles.aggasid01@gmail.com>
2022-01-14 12:35:05 -0600

    finished addContact method

--- src/ContactTesterApplication.java
+++ src/ContactTesterApplication.java
@@ -7,45 +7,11 @@ import java.util.Arrays;
 
 
 public class ContactTesterApplication {
-    public static void main(String[] args) {
-    // Creating the folder
-    String directory = "data";
-    String fileName = "contacts.txt";
+    public static void main(String[] args) throws IOException {
 
-    Path dataDirectory = Paths.get(directory);
-    Path dataFile = Paths.get(directory, fileName);
-
-        System.out.println(dataDirectory);
-        System.out.println(dataFile);
-
-        try {
-            if(Files.notExists(dataDirectory)){
-                Files.createDirectory(dataDirectory);
-            }
-            if(!Files.exists((dataFile))){
-                Files.createFile(dataFile);
-            }
-            if(Files.exists(dataFile)) {
-                // .write(Path filepath, List<String> list, StandardOpenOption.APPEND[optional])
-                Files.write(dataFile, Arrays.asList("Zeus", "Hercules", "Aphrodite", "Poseidon", "Hephaestus", "Athena"));
-                Files.write(dataFile, Arrays.asList("Hades", "Deimos", "Apollo"), StandardOpenOption.APPEND);
-            }
-        } catch (IOException iox){
-            iox.printStackTrace();
-        }
+        Methods.addContact();
 
 
-
-        try {
-            Files.write(
-                    Paths.get("dataFile", "contacts.txt"),
-                    Arrays.asList("eggs"), // list with one item
-                    StandardOpenOption.APPEND
-            );
-
-        } catch(IOException iox) {
-            iox.printStackTrace();
-        }
 
 
 
